b51f56511b0b7a3bab81207590b0785a388fa41f
tasnim-rahman <tasnimrahman0101@gmail.com>
2025-07-10 18:48:27 +0600

    ready for deployment

--- article/serializers.py
+++ article/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from article.models import Article, Category, Rating
+from django.contrib.auth import get_user_model
 
 
 class CategorySerializer(serializers.ModelSerializer):
@@ -12,7 +13,28 @@ class ArticleSerializer(serializers.ModelSerializer):
         model = Article
         fields = ['id', 'headline', 'body', 'category', 'publishing_time']
 
+class SimpleUserSerializer(serializers.ModelSerializer):
+    name = serializers.SerializerMethodField('get_current_username')
+    class Meta:
+        model = get_user_model()
+        fields = ['id', 'name']
+
+    def get_current_username(self, obj):
+        return obj.get_full_name()
+
 class RatingSerializer(serializers.ModelSerializer):
+    user = serializers.SerializerMethodField('get_user')
+
     class Meta:
         model = Rating
-        fields = ['id', 'rating', 'user']
+        fields = ['id', 'article', 'rating', 'user']
+        read_only_fields = ['article', 'user']
+        
+
+    def get_user(self, obj):
+        return SimpleUserSerializer(obj.user).data
+
+    def create(self, validated_data):
+        article = Article.objects.get(pk=self.context['article_id'])
+        validated_data['article'] = article
+        return super().create(validated_data)
--- article/views.py
+++ article/views.py
@@ -3,6 +3,7 @@ from article.serializers import ArticleSerializer, CategorySerializer, RatingSer
 from rest_framework.viewsets import ModelViewSet
 from article.paginations import DefaultPagination
 from api.permissions import IsAdminOrReadOnly
+from article.permissions import IsReviewAuthorOrReadOnly
 
 
 class ArticleViewSet(ModelViewSet):
@@ -17,8 +18,19 @@ class CategoryViewSet(ModelViewSet):
     permission_classes = [IsAdminOrReadOnly]
 
 class RatingViewSet(ModelViewSet):
-    queryset = Rating.objects.all()
     serializer_class = RatingSerializer
+    permission_classes = [IsReviewAuthorOrReadOnly]
 
-    def __str__(self):
-        return f"Rating by {self.user.first_name}."
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+    def perform_update(self, serializer):
+        serializer.save(user=self.request.user)
+
+    def get_queryset(self):
+        return Rating.objects.filter(article_id=self.kwargs['article_pk'])
+
+    def get_serializer_context(self):
+        context = super().get_serializer_context()
+        context['article_id'] = self.kwargs['article_pk']
+        return context
